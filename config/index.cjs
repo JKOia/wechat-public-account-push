02216b649cbf609fab8d7d26ac746d65a968e24d
JKOia <118803234+JKOia@users.noreply.github.com>
2024-05-29 23:07:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '左丹阳',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyam06ilST7Lsx58gmfIXtj59ycA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -40,14 +40,7 @@ const USER_CONFIG = {
         },
       ],
       // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
+      
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
